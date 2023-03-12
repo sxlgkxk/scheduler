6e1c077260b3b255617d4858d315acb3ec3c305b
sxlgkxk <sxlgkxk@gmail.com>
2023-03-12 12:42:38 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
-// import logo from './logo.svg';
-// import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   let [rowData, setRowData] = useState([]);
+  let [date, setDate] = useState(new Date());
+
+  let getRowKey = useCallback((i) => {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-input${i}`;
+  }, [date]);
 
   useEffect(() => {
     // 页面加载时，根据localStorage中的内容来填充表格
@@ -15,19 +31,21 @@ function App() {
     for (let i = 0; i < 24; i++) {
       rowData.push({
         key: i,
-        num: i,
-        input: localStorage.getItem(`input${i}`),
+        input: localStorage.getItem(getRowKey(i)) || "",
       });
     }
     setRowData(rowData);
-  }, []);
+  }, [getRowKey]);
 
   // 每当input框内容更改时，保存到localStorage中
   const handleChange = (e, key) => {
     let newRowData = rowData.map(item => {
       if (item.key === key) {
-        item.input = e.target.value;
-        localStorage.setItem(`input${item.key}`, item.input);
+        localStorage.setItem(getRowKey(key), e.target.value);
+        return {
+          ...item,
+          input: e.target.value
+        };
       }
       return item;
     });
@@ -35,29 +53,59 @@ function App() {
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <div className="App">
-        <DatePicker />
-        <table>
-          <tbody>
-            {rowData.map(item => {
-              return (
-                <tr key={item.key}>
-                  <td>{item.num}</td>
-                  <td>
-                    <input
-                      type="text"
-                      value={item.input}
-                      onChange={e => handleChange(e, item.key)}
-                    />
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </LocalizationProvider>
+
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <div className="App">
+          <DatePicker
+            sx={{ width: '60%' }}
+            value={date}
+            format="yyyy / MM / dd"
+            onChange={(newDate) => {
+              setDate(newDate);
+            }}
+          />
+          <table>
+            <tbody>
+            </tbody>
+          </table>
+          <Table sx={{ width: '100%' }} size="small">
+            <TableBody>
+              {rowData.map(item => {
+                return (
+                  <TableRow key={item.key}>
+                    <TableCell sx={{ width: '20px' }}>{item.key}</TableCell>
+                    <TableCell>
+                      <TextField
+                        // set width 100%
+                        sx={{
+                          width: '100%',
+                          '& .MuiInput-underline:before': {
+                            borderBottom: 'none',
+                          },
+                          '& .MuiInput-underline:after': {
+                            borderBottom: 'none',
+                          },
+                          '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+                            borderBottom: 'none',
+                          },
+                        }}
+                        variant="standard"
+                        type="text"
+                        value={item.input}
+                        onChange={(e) => handleChange(e, item.key)}
+                      />
+
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </LocalizationProvider>
+    </ThemeProvider>
   );
 }
 
